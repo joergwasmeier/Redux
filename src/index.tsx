36eb9ced1d55020d529af9c704e1a1b44a1ce3e6
Jörg Wasmeier <j.wasmeier@faba.de>
2017-12-27 10:22:23 +0100

    Middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import {combineReducers, createStore} from 'redux';
+import {applyMiddleware, combineReducers, createStore, Middleware} from 'redux';
 import userReducer from './reducers/userReducer';
 import tweetsReducer from './reducers/tweetsReducer';
 
@@ -7,7 +7,22 @@ const reducers = combineReducers({
     tweets:tweetsReducer,
 });
 
-const store = createStore(reducers);
+const logger = (store) => (next) => (action) => {
+    console.log("action fired", action);
+    return next(action);
+};
+
+const error : Middleware = api => next => action => {
+    try {
+        return next(action);
+    } catch (e){
+        throw "ahh error";
+    }
+};
+
+const middleware = applyMiddleware(logger, error);
+
+const store = createStore(reducers, middleware);
 
 store.subscribe(() => {
     console.log("store changed", store.getState());
